04e03dffe396c1cb0559e334df0f68edf41bfa6c
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2025-09-17 21:15:59 -0300

    fase 2.1 concluida - edit e delete

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -9,12 +9,15 @@ import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 import android.content.Intent
+import android.view.ContextMenu
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
-
-
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 
 
 class MainActivity : AppCompatActivity() {
@@ -42,11 +45,13 @@ class MainActivity : AppCompatActivity() {
         carl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK){
                 val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
-                contact?.also {
-                    if(contactList.any{it.id == contact.id}){ //se o contato ja esta na lista
-                        //editar
+                contact?.also {newOrEditedContact ->
+                    if(contactList.any{it.id == newOrEditedContact.id}){ //se o contato ja esta na lista
+                      val position = contactList.indexOfFirst { it.id == newOrEditedContact.id }
+                        contactList[position] = newOrEditedContact
+                    //editar
                 } else {
-                    contactList.add(contact)
+                    contactList.add(newOrEditedContact)
 
                 }
                 contactAdapter.notifyDataSetChanged() //e vai notificvar o adaptter dessa modificacao
@@ -57,6 +62,14 @@ class MainActivity : AppCompatActivity() {
         fillContacts()
 
         amb.contactLv.adapter = contactAdapter
+        registerForContextMenu(amb.contactLv) //aqui é o click LONGO
+
+        amb.contactLv.setOnItemClickListener { _, _, position, _ -> //aqui o click curto
+            startActivity(Intent(this, ContactActivity::class.java).apply {
+                putExtra(EXTRA_CONTACT, contactList[position])
+                putExtra(EXTRA_VIEW_CONTACT, true)
+            })
+        }
     }
 
 
@@ -73,6 +86,45 @@ class MainActivity : AppCompatActivity() {
             }
             else -> {false}
         }
+    }
+
+    //funcao para ciar o menu de contexto EDIT e REMOVE
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_menu_main, menu)
+    }
+
+    //funcao para tratar o clique no menu de contexto
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+
+       return when(item.itemId){
+           R.id.removeContactMi -> {
+               //adcninando a funcionalidade do remove
+               contactList.removeAt(position)
+               contactAdapter.notifyDataSetChanged()
+               Toast.makeText(this, "Contact removido", Toast.LENGTH_SHORT).show()
+               true
+           }
+           R.id.editContactMi -> {
+               //adcninando a funcionalidade do edit
+               carl.launch(Intent(this, ContactActivity::class.java).apply {
+                   putExtra(EXTRA_CONTACT, contactList[position])
+                   putExtra(EXTRA_VIEW_CONTACT, false)
+
+               })
+               true
+           }
+           else -> {false}
+       }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterForContextMenu(amb.contactLv)
     }
 
 
